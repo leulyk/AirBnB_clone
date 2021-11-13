2192987a2f468a3b7c58af6d12abda6ee4fd324a
Leul Yiheyis <leulykpro@gmail.com>
2021-11-13 12:04:46 +0300

    check attributes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,8 +7,14 @@ from unittest.mock import patch
 import io
 from models.engine.file_storage import FileStorage
 
+
 class TestFileStorage(unittest.TestCase):
     """ class providing unit tests for the FileStorage class """
     def setUp(self):
         """ initializes a FileStorage instance for testing """
-        test_storage = FileStorage()
+        self.test_storage = FileStorage()
+
+    def test_check_attributes(self):
+        """ checks for mandatory attributes in the FileStorage class """
+        self.assertTrue(hasattr(self.test_storage, "_FileStorage__file_path"))
+        self.assertTrue(hasattr(self.test_storage, "_FileStorage__objects"))
